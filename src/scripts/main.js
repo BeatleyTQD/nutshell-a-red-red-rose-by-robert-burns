6b5deb9e0c662e59c33e11c78d10e6f6d301a89d
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-29 14:12:04 -0500

    changed news data source

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,7 +24,7 @@ async function start(){
     if(window.sessionStorage.activeUser) {
         TopSectionTemplate();
         TaskCardGenerator(data.tasks);
-        NewsTemplate();
+        NewsTemplate(data.news);
         EventListeners.setStandard();
     } else{
         renderLogin()
